400393de70697d71a09a17535c55820ca6cffee0
Natawut Nupairoj <natawut.n@chula.ac.th>
2025-03-23 16:58:58 +0700

    Revise Opstars slides and codes

--- code/Week10_OpsStars/airflow/dsde_simplebash2.py
+++ code/Week10_OpsStars/airflow/dsde_simplebash2.py
@@ -3,7 +3,11 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 # declare a DAG
-with DAG(dag_id='dsde_simplebash', start_date=days_ago(1)):
+with DAG(dag_id='dsde_simplebash_2', start_date=days_ago(1)):
 
-	# declare a TASK
+	# declare 2 TASKS
 	echo = BashOperator(task_id='echo_template', bash_command='echo "run_id = {{ run_id }} and ds = {{ ds }}"')
+	echo2 = BashOperator(task_id='echo_template_2', bash_command='echo "run_id_2 = {{ run_id }} and ds = {{ ds }}"')
+
+	# declare TASK dependency
+	echo >> echo2
